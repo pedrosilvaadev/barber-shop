eaacfc995d30ad49cdd29cc62a4d10652e2d27b2
Pedro Silva <pedro_rike@hotmail.com.br>
2026-01-22 23:05:44 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
-  return <h1 className="mb-5 bg-red-400">Hello world</h1>
+  return <Button>Hello world</Button>
 }
