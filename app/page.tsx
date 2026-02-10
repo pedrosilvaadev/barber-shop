7425e7d1a6122ce5bd65a6ba6a32706d5e3589e6
Pedro Silva <pedro_rike@hotmail.com.br>
2026-02-09 23:25:12 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
+import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Hello world</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Pedro!</h2>
+        <p>Segunda-feira, 09 de fevereiro.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[180px] w-full">
+          <Image
+            src="/images/banner-01.svg"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
